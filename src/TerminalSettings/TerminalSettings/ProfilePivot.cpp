1d20444a832ea1629e72c203348e788b59191c8c
Carlos Zamora <carlos.zamora@microsoft.com>
2020-07-24 16:49:35 -0400

    Add a pivot control

---
+++ src/TerminalSettings/TerminalSettings/ProfilePivot.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "ProfilePivot.h"
+#include "ProfilePivot.g.cpp"
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::SettingsControl::implementation
+{
+    ProfilePivot::ProfilePivot()
+    {
+        InitializeComponent();
+    }
+
+    int32_t ProfilePivot::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void ProfilePivot::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void ProfilePivot::ClickHandler(IInspectable const&, RoutedEventArgs const&)
+    {
+
+    }
+}
